fd6121511c8b32d5c4e393ff0c42c569de93bbf0
Dimitar Petkov <75310146+Dimitar33@users.noreply.github.com>
2020-12-03 11:13:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _001._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            
+            int big = 0;
+            int mid = 0;
+            int low = 0;
+
+            big = Math.Max(Math.Max(n1, n2), n3);
+            low = Math.Min(Math.Min(n1, n2), n3);
+            mid = (n1 + n2 + n3) - (big + low);
+
+            Console.WriteLine(big);
+            Console.WriteLine(mid);
+            Console.WriteLine(low);
+        }
+    }
+}
