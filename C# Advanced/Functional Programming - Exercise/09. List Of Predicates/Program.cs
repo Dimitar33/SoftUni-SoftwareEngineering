9efdac20924463ddfcabc03cb06467f349f16f91
Dimitar33 <DimPetkov777@gmail.com>
2021-01-06 14:50:08 +0200

    FP

--- C# Advanced/Functional Programming - Exercise/09. List Of Predicates/Program.cs
+++ C# Advanced/Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -9,35 +9,32 @@ namespace _09._List_Of_Predicates
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] nums = Console.ReadLine().Split().Distinct().Select(int.Parse).ToArray();
 
             List<int> list = new List<int>();
 
+
             for (int i = 1; i <= n; i++)
             {
-                list.Add(i);
-            }
+                bool isDevisible = true;
 
-            list = list.Where(c =>
-            {
-                int num = 0;
-                bool flag = false;
-
-                for (int i = 0; i < list.Count; i++)
+                foreach (var item in nums)
                 {
-                    for (int j = 0; j < nums.Length; j++)
+                    Predicate<int> predicate = c => i % c != 0;
+
+                    if (predicate(item))
                     {
-                        if (nums[j] % i != 0)
-                        {
-                            flag = true;
-                            break;
-                        }
-
+                        isDevisible = false;
+                        break;
                     }
-                }
 
-                return
-            })
+                }
+                if (isDevisible)
+                {
+                    list.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
         }
     }
 }
---
+++ C# Advanced/Functional Programming - Exercise/10. Predicate Party!/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _10._Predicate_Party_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pplComing = Console.ReadLine().Split().ToList();
+
+            string[] cmd = Console.ReadLine().Split();
+
+
+
+            while (cmd[0] != "Party!")
+            {
+                if (cmd[0] == "Remove")
+                {
+                    if (cmd[1] == "StartsWith")
+                    {
+
+                        pplComing = pplComing.Where(c => !c.StartsWith(cmd[2])).ToList();
+                    }
+                    else if (cmd[1] == "EndsWith")
+                    {
+                        pplComing = pplComing.Where(c => !c.EndsWith(cmd[2])).ToList();
+                    }
+                    else
+                    {
+                        pplComing = pplComing.Where(c => !(c.Length == int.Parse(cmd[2]))).
+                            ToList();
+                    }
+
+                }
+                else
+                {
+                    List<string> tempList = new List<string>();
+
+                    if (cmd[1] == "StartsWith")
+                    {
+                        pplComing.AddRange(pplComing.Where(c => c.StartsWith(cmd[2])).
+                            ToList());
+                    }
+                    else if (cmd[1] == "EndsWith")
+                    {
+                        pplComing.AddRange(pplComing.Where(c => c.EndsWith(cmd[2])).ToList());
+                    }
+                    else
+                    {
+                        pplComing.AddRange(pplComing.Where(c => c.Length == int.Parse(cmd[2])).
+                            ToList());
+                    }
+
+
+                }
+                cmd = Console.ReadLine().Split();
+            }
+
+          
+
+            if (pplComing.Count == 0)
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+            else
+            {
+                Console.WriteLine( $"{string.Join(", ", pplComing)} are going to the party!");
+            }
+        }
+    }
+}
