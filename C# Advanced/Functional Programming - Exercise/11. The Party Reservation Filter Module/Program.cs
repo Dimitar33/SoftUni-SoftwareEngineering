4ed86a049e243985a0f6715e285ece737c788b3d
Dimitar33 <DimPetkov777@gmail.com>
2021-01-07 18:43:38 +0200

    FP

---
+++ C# Advanced/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guestList = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string[] cmd = Console.ReadLine().Split(";");
+
+            Dictionary<string, List<string>> filters = new Dictionary<string, List<string>>();
+
+            while (cmd[0] != "Print")
+            {
+                if (cmd[0] == "Add filter")
+                {
+                    if (!filters.ContainsKey(cmd[1]))
+                    {
+                        filters.Add(cmd[1], new List<string>());
+                    }
+                    filters[cmd[1]].Add(cmd[2]);
+                }
+                else
+                {
+                    filters[cmd[1]].Remove(cmd[2]);
+
+                    if (filters[cmd[1]].Count == 0)
+                    {
+                        filters.Remove(cmd[1]);
+                    }
+                }
+
+                cmd = Console.ReadLine().Split(";");
+            }
+            foreach (var item in filters)
+            {
+                if (item.Key == "Starts with")
+                {
+                    foreach (var i in item.Value)
+                    {
+                        guestList = guestList.Where(c => !c.StartsWith(i)).ToList();
+                    }
+                }
+                else if (item.Key == "Ends with")
+                {
+                    foreach (var i in item.Value)
+                    {
+                        guestList = guestList.Where(c => !c.EndsWith(i)).ToList();
+                    }
+                }
+                else if (item.Key == "Length")
+                {
+                    foreach (var i in item.Value)
+                    {
+                        guestList = guestList.Where(c => c.Length != int.Parse(i)).ToList();
+                    }
+                }
+                else
+                {
+                    foreach (var i in item.Value)
+                    {
+                        guestList = guestList.Where(c => !c.Contains(i)).ToList();
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", guestList));
+        }
+    }
+}
---
+++ C# Advanced/Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split().ToList();
+            string name = "";          
+            
+            name = names.First(c =>
+            {
+                bool isTrue = false;
+                int sum = 0;
+
+                foreach (var item in c)
+                {
+                    sum += item;
+                }
+
+                if (sum >= n)
+                {
+                    isTrue = true;
+                    return isTrue;
+                }
+                
+                return isTrue;
+            }).ToString();
+
+            Console.WriteLine(name);
+
+        }
+    }
+}
