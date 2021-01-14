a81da225158777f549acf79ec5a0d343383fd186
Dimitar33 <DimPetkov777@gmail.com>
2021-01-14 17:54:08 +0200

    Create Custom Data Structures

---
+++ C# Advanced/Create Custom Data Structures/Create Custom Data Structures/CustomList.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Create_Custom_Data_Structures
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] array;
+
+        public int Count { get; set; }
+
+        private bool invalidIndex(int index)
+           => index < 0 || index >= Count;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (invalidIndex(index))
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return array[index];
+            }
+            set
+            {
+                if (invalidIndex(index))
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                array[index] = value;
+            }
+        }
+        public CustomList()
+        {
+            array = new int[InitialCapacity];
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[array.Length * 2];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+            array = copy;
+        }
+
+      
+
+        public void Add(int item)
+        {
+            if (Count == array.Length)
+            {
+                Resize();
+            }
+            array[Count] = item;
+            Count++;
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[array.Length / 2];
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+            array = copy;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < array.Length -1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+             
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (invalidIndex(index))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int temp = array[index];
+
+            ShiftLeft(index);
+            Count--;
+
+            if (Count * 4 <= array.Length)
+            {
+                Shrink();
+            }
+
+            return temp;         
+
+        }
+        private void ShiftRight(int index)
+        {
+            
+            for (int i = 0; i < Count - index ; i++)
+            {
+                array[Count - i] = array[Count - i - 1];
+            }
+        }
+        public void Insert(int index, int item)
+        {
+            if (invalidIndex(index))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            ShiftRight(index);
+
+            array[index] = item;
+            Count++;
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {           
+                if (array[i] == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int first, int second)
+        {
+            if (invalidIndex(first) || invalidIndex(second))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
---
+++ C# Advanced/Create Custom Data Structures/Create Custom Data Structures/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Create_Custom_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CustomList list = new CustomList();
+
+            //list.Add(2);
+            //list.Add(0);
+            //list.Add(5);
+            //list.Add(3);
+            //list.Add(9);
+            //list.Insert(3, 12);
+
+            //Console.WriteLine(list[6]);
+
+            //list.Swap(2, 4);
+
+            //Console.WriteLine(list.Contains(2));
+            //Console.WriteLine(list.Contains(0));
+            //list.Contains(0);
+
+
+            //list.RemoveAt(1);
+            //list.RemoveAt(3);
+
+            CustomStack stack = new CustomStack();
+            
+
+            stack.Push(4);
+            stack.Push(7);
+            stack.Push(1);
+            stack.Push(1);
+            stack.Push(1);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Peek());
+            stack.Push(2);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
