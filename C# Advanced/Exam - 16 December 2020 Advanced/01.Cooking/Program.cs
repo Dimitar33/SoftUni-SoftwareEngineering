25d828f97520a7d54244612c87191210d408a55e
Dimitar33 <DimPetkov777@gmail.com>
2021-02-08 17:08:28 +0200

    Exam - 16 December 2020 Advanced

---
+++ C# Advanced/Exam - 16 December 2020 Advanced/01.Cooking/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liquidsInput = Console.ReadLine().Split().
+                Select(int.Parse).ToArray();
+            int[] ingridientsInput = Console.ReadLine().
+                Split().Select(int.Parse).ToArray();
+
+            Queue<int> liquids = new Queue<int>();
+            Stack<int> ingrids = new Stack<int>();
+
+            int bread = 0;
+            int cake = 0;
+            int fruitPie = 0;
+            int pastry = 0;
+
+            for (int i = 0; i < liquidsInput.Length; i++)
+            {
+                liquids.Enqueue(liquidsInput[i]);
+            }
+            for (int i = 0; i < ingridientsInput.Length; i++)
+            {
+                ingrids.Push(ingridientsInput[i]);
+            }
+
+            while (liquids.Count > 0 && ingrids.Count > 0)
+            {
+                int liquid = liquids.Dequeue();
+                int ingrid = ingrids.Pop();
+
+                if (liquid + ingrid == 25)
+                {
+                    bread++;
+                }
+                else if (liquid + ingrid == 50)
+                {
+                    cake++;
+                }
+                else if (liquid + ingrid == 75)
+                {
+                    pastry++;
+                }
+                else if (liquid + ingrid == 100)
+                {
+                    fruitPie++;
+                }
+                else
+                {
+                    ingrids.Push(ingrid + 3);
+                }
+            }
+
+            if (bread > 0 && cake > 0 && pastry > 0 && fruitPie > 0)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+            if (liquids.Count == 0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            if (ingrids.Count == 0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingrids)}");
+            }
+            Console.WriteLine($"Bread: {bread}");
+            Console.WriteLine($"Cake: {cake}");
+            Console.WriteLine($"Fruit Pie: {fruitPie}");
+            Console.WriteLine($"Pastry: {pastry}");
+        }
+    }
+}
---
+++ C# Advanced/Exam - 16 December 2020 Advanced/02.Selling/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] bakery = new char[n, n];
+            int x = 0;
+            int y = 0;
+            int money = 0;
+            bool isOut = false;
+            int pilar1y = 0;
+            int pilar1x = 0;
+            int pilar2y = 0;
+            int pilar2x = 0;
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] fill = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < n; j++)
+                {
+                    bakery[i, j] = fill[j];
+
+                    if (fill[j] == 'S')
+                    {
+                        y = i;
+                        x = j;
+                    }
+                    if (fill[j] == 'O')
+                    {
+                        if (count == 0)
+                        {
+                            pilar1y = i;
+                            pilar1x = j;
+                        }
+                        else
+                        {
+                            pilar2y = i;
+                            pilar2x = j;
+                        }
+                    }
+                }
+            }
+
+            string move = Console.ReadLine();
+
+            while (money < 50 && isOut == false)
+            {           
+
+                if (move == "left" && x - 1 >= 0)
+                {
+
+                    if (bakery[y, x - 1] == 'O')
+                    {
+                        bakery[y, x - 1] = '-';
+                        bakery[x, y] = '-';
+
+                        if (y == pilar1y && x - 1 == pilar1x)
+                        {
+                            y = pilar2y;
+                            x = pilar2x;
+                        }
+                        else
+                        {
+                            y = pilar1y;
+                            x = pilar1x;
+                        }
+                        bakery[y, x] = 'S';
+                    }
+                    else if (bakery[y, x - 1] == '-')
+                    {
+                        bakery[y, x - 1] = 'S';
+                        bakery[y, x] = '-';
+                        x--;
+                    }
+                    else
+                    {
+                        money += bakery[y, x - 1] - '0';
+                        bakery[y, x - 1] = 'S';
+                        bakery[y, x] = '-';
+                        x--;
+                    }
+                }
+                else if (move == "right" && x + 1 < bakery.GetLength(1))
+                {
+                    if (bakery[y, x + 1] == 'O')
+                    {
+                        bakery[y, x + 1] = '-';
+                        bakery[y, x] = '-';
+
+                        if (y == pilar1y && x + 1 == pilar1x)
+                        {
+                            y = pilar2y;
+                            x = pilar2x;
+                        }
+                        else
+                        {
+                            y = pilar1y;
+                            x = pilar1x;
+                        }
+                        bakery[y, x] = 'S';
+                    }
+                    else if (bakery[y, x + 1] == '-')
+                    {
+                        bakery[y, x + 1] = 'S';
+                        bakery[y, x] = '-';
+                        x++;
+                    }
+                    else
+                    {
+                        money += bakery[y, x + 1] - '0';
+                        bakery[y, x + 1] = 'S';
+                        bakery[y, x] = '-';
+                        x++;
+                    }
+                }
+                else if (move == "up" && y - 1 >= 0)
+                {
+                    if (bakery[y - 1, x] == 'O')
+                    {
+                        bakery[y - 1, x] = '-';
+                        bakery[y, x] = '-';
+
+                        if (y - 1 == pilar1y && x == pilar1x)
+                        {
+                            y = pilar2y;
+                            x = pilar2x;
+                        }
+                        else
+                        {
+                            y = pilar1y;
+                            x = pilar1x;
+                        }
+                        bakery[y, x] = 'S';
+                    }
+                    else if (bakery[y - 1, x] == '-')
+                    {
+                        bakery[y - 1, x] = 'S';
+                        bakery[y, x] = '-';
+                        y--;
+                    }
+                    else
+                    {
+                        money += bakery[y - 1, x] - '0';
+                        bakery[y - 1, x] = 'S';
+                        bakery[y, x] = '-';
+                        y--;
+                    }
+                }
+                else if (move == "down" && y + 1 < bakery.GetLength(0))
+                {
+                    if (bakery[y + 1, x] == 'O')
+                    {
+                        bakery[y + 1, x] = '-';
+                        bakery[y, x] = '-';
+
+                        if (y + 1 == pilar1y && x == pilar1x)
+                        {
+                            y = pilar2y;
+                            x = pilar2x;
+                        }
+                        else
+                        {
+                            y = pilar1y;
+                            x = pilar1x;
+                        }
+                        bakery[y, x] = 'S';
+                    }
+                    else if (bakery[y + 1, x] == '-')
+                    {
+                        bakery[y + 1, x] = 'S';
+                        bakery[y, x] = '-';
+                        y++;
+                    }
+                    else
+                    {
+                        money += bakery[y + 1, x] - '0';
+                        bakery[y + 1, x] = 'S';
+                        bakery[y, x] = '-';
+                        y++;
+                    }
+                }
+                else
+                {
+
+                    isOut = true;
+                    break;
+                }
+              
+                move = Console.ReadLine();
+
+            }
+
+            if (isOut)
+            {
+                Console.WriteLine("Bad news, you are out of the bakery.");
+                Console.WriteLine($"Money: {money}");
+                bakery[y, x] = '-';
+            }
+            else
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+                Console.WriteLine($"Money: {money}");
+            }
+
+            for (int i = 0; i < bakery.GetLength(0); i++)
+            {
+                for (int j = 0; j < bakery.GetLength(1); j++)
+                {
+                    Console.Write(bakery[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
