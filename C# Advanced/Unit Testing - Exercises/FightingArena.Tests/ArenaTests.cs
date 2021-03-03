6eb9f6a8babf031a7e29faa973476a7f858f1c91
Dimitar33 <DimPetkov777@gmail.com>
2021-03-03 15:43:26 +0200

    FightingArena.Tests

--- C# Advanced/Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
+++ C# Advanced/Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿//using FightingArena;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -10,9 +13,72 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void CtorWorkCorect()
+        {          
+            Arena arena = new Arena();
+
+            var list = new List<Warrior>();
+
+            CollectionAssert.AreEqual(list, arena.Warriors);
+            Assert.AreEqual(0, arena.Count);
+        }
+
+        [Test]
+        public void EnrollWarriorAlreadyEnroled()
         {
-            Assert.Pass();
+            Warrior warr = new Warrior("Illidan", 100, 200);           
+
+            Arena arena = new Arena();
+            arena.Enroll(warr);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(warr));
+        }
+
+        [Test]
+        public void EnrollWorkCorect()
+        {
+            Warrior warr = new Warrior("Illidan", 100, 200);
+            Warrior warr2 = new Warrior("Guldan", 100, 300);
+
+            Arena arena = new Arena();
+            arena.Enroll(warr);
+            arena.Enroll(warr2);
+
+            var list = new List<Warrior>{warr, warr2};
+
+            CollectionAssert.AreEqual(list, arena.Warriors);
+            Assert.AreEqual(2, arena.Count);
+        }
+
+        [Test]
+        public void FightMissingFigterError()
+        {
+            Warrior warr = new Warrior("Illidan", 100, 200);
+            Warrior warr2 = new Warrior("Guldan", 100, 300);
+
+            Arena arena = new Arena();
+            arena.Enroll(warr);
+         
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Illidan", "Guldan"));
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Guldan", "Illidan"));
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Ivan", "Gosho"));
+         
+        }
+
+        [Test]
+        public void FightkWorkCorect()
+        {
+            Warrior warr = new Warrior("Illidan", 100, 200);
+            Warrior warr2 = new Warrior("Guldan", 100, 300);
+
+            Arena arena = new Arena();
+            arena.Enroll(warr);
+            arena.Enroll(warr2);
+
+            arena.Fight("Illidan", "Guldan");
+
+            Assert.AreEqual(100, warr.HP);
+            Assert.AreEqual(200, warr2.HP);
         }
     }
 }
--- C# Advanced/Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
+++ C# Advanced/Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,4 +1,6 @@
+//using FightingArena;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
@@ -10,9 +12,115 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase("Illidan", 120, 1110)]
+        [TestCase("Guldan", 140, 2210)]
+        [TestCase("Aziro", 60, 0)]
+        public void CtorWorkingCorectly(string name, int dps, int hp)
         {
-            Assert.Pass();
+            Warrior warr = new Warrior(name, dps, hp);
+
+            Assert.AreEqual(name, warr.Name);
+            Assert.AreEqual(dps, warr.Damage);
+            Assert.AreEqual(hp, warr.HP);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase(" ")]
+        [TestCase("   ")]
+        [TestCase("")]
+        public void NameIsNullOrEmpty(string name)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            new Warrior(name , 122, 1222));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]       
+        public void DamageIsNegativeOrZero(int dps)
+        {
+            Assert.Throws<ArgumentException>(() => 
+            new Warrior("Illidan", dps, 1222));
+        }
+        [Test]            
+        [TestCase(-5)]      
+        [TestCase(-1)]      
+        public void HPIsLessThanZero(int hp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            new Warrior("Illidan", 121, hp));
+        }
+        [Test]
+        [TestCase(21)]      
+        [TestCase(30)]
+        public void AttackerHPLessThanMinimum(int hp)
+        {
+            Warrior warr = new Warrior("Illidan", 121, hp);
+            Warrior warr1 = new Warrior("Guldan", 121, 200);
+
+            Assert.Throws<InvalidOperationException>(() => warr.Attack(warr1));
+       
+            //May be more
+        }
+        [Test]
+        [TestCase(21)]    
+        [TestCase(30)]    
+        public void DefenderHPLessThanMinimum(int hp)
+        {
+            Warrior warr = new Warrior("Illidan", 121, 200);
+            Warrior warr1 = new Warrior("Guldan", 121, hp);
+
+            Assert.Throws<InvalidOperationException>(() => warr.Attack(warr1));
+
+            //May be more
+        }
+
+        [Test]
+        [TestCase(67)]
+        [TestCase(41)]
+        [TestCase(50)]
+        public void CantAttackStrongerEnemy(int dmg)
+        {
+            Warrior warr = new Warrior("Illidan", 10, 40);
+            Warrior warr1 = new Warrior("Guldan", dmg, 40);
+
+            Assert.Throws<InvalidOperationException>(() => warr.Attack(warr1));
+
+            //May be more
+        }
+        [Test]      
+        public void AttackWorksCorect()
+        {
+            Warrior warr1 = new Warrior("Illidan", 100, 200);
+            Warrior warr2 = new Warrior("Guldan", 100, 300);           
+
+            warr1.Attack(warr2);
+           
+            var expectedWarr1Hp = 100;
+            var expectedWarr2Hp = 200;
+            
+            Assert.AreEqual(expectedWarr1Hp, warr1.HP);
+            Assert.AreEqual(expectedWarr2Hp, warr2.HP);
+            
+           
+        }
+
+        [Test]
+        [TestCase(50, 100)]
+        [TestCase(100, 300)]
+        public void AttackWorksCorectWithMoreDmg(int dmg, int hp)
+        {
+            Warrior warr1 = new Warrior("Illidan", dmg, hp);
+            Warrior warr2 = new Warrior("Guldan", 10, 50);
+            
+
+            warr1.Attack(warr2);            
+          
+            var expectedWarr3Hp = 0;
+
+            Assert.AreEqual(expectedWarr3Hp, warr2.HP);
+
         }
     }
 }
