8d6d806980315e479ac583dab14cf3425e4ae960
Dimitar33 <DimPetkov777@gmail.com>
2021-03-17 12:52:38 +0200

    Databases Introduction

--- C# DB/Databases Introduction/Databases Introduction.sql
+++ C# DB/Databases Introduction/Databases Introduction.sql
@@ -1,4 +1,4 @@
-
+﻿
 			-- 1. Create Database
 
 CREATE DATABASE Minions
@@ -408,14 +408,111 @@ INSERT INTO Occupancies (EmployeeId, DateOccupied, RoomNumber) VALUES
  (2, '1-4-2012', 3),
  (3, '12-2-2012', 2)
 
+				-- 16. Create SoftUni Database
 
+CREATE DATABASE SoftUni
 
+CREATE TABLE Towns 
+(
+ Id INT PRIMARY KEY IDENTITY,
+ [Name] VARCHAR (50) NOT NULL
+)
 
+CREATE TABLE Addresses 
+(
+ Id INT PRIMARY KEY IDENTITY, 
+ AddressText VARCHAR (200) NOT NULL, 
+ TownId INT FOREIGN KEY REFERENCES Towns(Id)
+)
+
+CREATE TABLE Departments 
+(
+ Id INT PRIMARY KEY IDENTITY, 
+ [Name] VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Employees 
+(
+ Id INT PRIMARY KEY IDENTITY, 
+ FirstName VARCHAR(30) NOT NULL, 
+ MiddleName VARCHAR(30), 
+ LastName VARCHAR(30) NOT NULL, 
+ JobTitle VARCHAR(50) NOT NULL, 
+ DepartmentId INT FOREIGN KEY REFERENCES Departments(Id), 
+ HireDate DATETIME NOT NULL, 
+ Salary DECIMAL(9,2) NOT NULL, 
+ AddressId INT FOREIGN KEY REFERENCES Addresses(Id)
+)
+
+				-- 17. Backup Database
+
+				-- 18. Basic Insert
+
+INSERT INTO Towns ([Name]) VALUES
+ ('Sofia'), 
+ ('Plovdiv'), 
+ ('Varna'), 
+ ('Burgas')
+
+INSERT INTO Departments ([Name]) VALUES
+ ('Engineering'), 
+ ('Sales'), 
+ ('Marketing'), 
+ ('Software Development'),
+ ('Quality Assurance')
+
+INSERT INTO Employees (FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary) VALUES
+ ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer',	4,	'01/02/2013',	3500.00),
+ ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '02/03/2004', 4000.00),
+ ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '28/08/2016', 525.25),
+ ('Georgi', 'Teziev', 'Ivanov',	'CEO', 2, '09/12/2007', 3000.00),
+ ('Peter', 'Pan', 'Pan', 'Intern', 3, '28/08/2016', 599.88)
+
+				-- 19. Basic Select All Fields
+
+SELECT * FROM Towns
+SELECT * FROM Departments
+SELECT * FROM Employees
+
+				-- 20. ⦁	Basic Select All Fields and Order Them
+
+SELECT * FROM Towns
+ORDER BY [Name]
+
+SELECT * FROM Departments
+ORDER BY [Name]
+
+SELECT * FROM Employees
+ORDER BY Salary DESC 
+
+				-- 21. Basic Select Some Fields
+
+SELECT [Name] FROM Towns
+ORDER BY [Name]
+
+SELECT [Name] FROM Departments
+ORDER BY [Name]
+
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+ORDER BY Salary DESC 
+
+				-- 22. Increase Employees Salary
+
+USE SoftUni
+UPDATE Employees
+SET Salary *= 1.1
+SELECT Salary FROM Employees
+ORDER BY Salary DESC 
+
+				-- 23. Decrease Tax Rate
+
+USE Hotel
+UPDATE Payments
+SET TaxRate = 0.97;
+SELECT TaxRate FROM Payments
+
+				-- 24. Delete All Records
+
+USE Hotel
+DELETE  Occupancies
 SELECT * FROM Occupancies
-
-
-
-
-
-
-
