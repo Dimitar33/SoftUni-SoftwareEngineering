1e1a40dd7c88a623083e7c5522359946a1cc97fa
Dimitar33 <DimPetkov777@gmail.com>
2021-04-22 13:12:02 +0300

    Entity Framework Introduction

---
+++ C# DB Entity Framework Core/Entity Framework Introduction/03. Employees Full Information/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext softuni = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(softuni));
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.OrderBy(x => x.EmployeeId);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName} {item.MiddleName} {item.JobTitle} {item.Salary:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# DB Entity Framework Core/Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext softuni = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(softuni));
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees.Where(x => x.Salary > 50000).OrderBy(x => x.FirstName);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} - {item.Salary:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
