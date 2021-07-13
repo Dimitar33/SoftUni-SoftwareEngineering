2717ac77b54ac95b4b05f2e0b24af04ac952cb84
Dimitar33 <DimPetkov777@gmail.com>
2021-07-13 16:26:59 +0300

    added forms for adding pets

---
+++ ASP.NET Core/PetsDates/PetsDates/Models/Cats/AddCatViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PetsDates.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static PetsDates.Data.DataConstants;
+
+namespace PetsDates.Models.Cats
+{
+    public class AddCatViewModel
+    {
+        [Required]
+        public CatBreed Breed { get; set; }
+
+        [Required]
+        [MaxLength(PetNameMaxLenght)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [Url]
+        public string PictureUrl { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
---
+++ ASP.NET Core/PetsDates/PetsDates/Models/Dogs/AddDogViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PetsDates.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static PetsDates.Data.DataConstants;
+
+namespace PetsDates.Models.Dogs
+{
+    public class AddDogViewModel
+    {
+        [Required]
+        public DogBreed Breed { get; set; }
+
+        [Required]
+        [MaxLength(PetNameMaxLenght)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [Url]
+        public string PictureUrl { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
