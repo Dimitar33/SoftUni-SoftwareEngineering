843419211ebe53eb3e529aab8f53d2d6a99c8bbc
Dimitar33 <DimPetkov777@gmail.com>
2021-08-16 10:57:52 +0300

    Users can login with username not email

--- ASP.NET Core/PetsDates/PetsDates/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ASP.NET Core/PetsDates/PetsDates/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -40,7 +40,7 @@ namespace PetsDates.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            public string Email { get; set; }
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -70,7 +70,7 @@ namespace PetsDates.Areas.Identity.Pages.Account
         
             if (ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
